4c5c338d55c3b64f70abf5125428a3c7639394f4
zakkazmanli <zakariyyaakazmanli@gmail.com>
2021-02-09 11:11:01 +0000

    Add raindrop class

---
+++ RainDrops/RainDrops/RainDrop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RainDrops
+{
+    public class RainDrop
+    {
+        public string ConvertNumToRainDropString(int num)
+        {
+            string output = "";
+
+            if (num % 3 == 0) { output += "Pling"; }
+            if (num % 5 == 0) { output += "Plang"; }
+            if (num % 7 == 0) { output += "Plong"; }
+
+            if (string.IsNullOrEmpty(output)) { output = num.ToString(); }
+            return output;
+        }
+    }
+}
